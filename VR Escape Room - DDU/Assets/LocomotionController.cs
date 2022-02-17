76fe0215ee0b0b84a1cd830167066db6a882b4cf
jayc545 <54627710+jayc545@users.noreply.github.com>
2022-02-17 21:03:07 +0100

    sdfsd

--- VR Escape Room - DDU/Assets/LocomotionController.cs
+++ VR Escape Room - DDU/Assets/LocomotionController.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class LocomotionController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public XRController leftTeleportRay;
+    public XRController rightTeleportRay;
+    public InputHelpers.Button teleportActivationButton;
+    public float acticationThreshTold = 0.1f;
+
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (leftTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(leftTeleportRay));
+        }
+        if (rightTeleportRay)
+        {
+            leftTeleportRay.gameObject.SetActive(CheckIfActivated(rightTeleportRay));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivationButton, out bool isActivated, acticationThreshTold);
+        return isActivated;
     }
 }
