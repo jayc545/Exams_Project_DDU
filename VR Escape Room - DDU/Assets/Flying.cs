0c86b482042930d28f0844f84cb7a753b008723a
Jakie <jayc545htx@gmail.com>
2022-03-11 12:39:52 +0100

    Added thing

--- VR Escape Room - DDU/Assets/Flying.cs
+++ VR Escape Room - DDU/Assets/Flying.cs
@@ -6,7 +6,7 @@ public class Flying : MonoBehaviour
 {
     public MeshRenderer itemModel;
 
-    public float itemRotationSpeed = 50f;
+    public float itemRotationSpeed =50f;
     public float itemBobSpeed = 2f;
     private Vector3 basePosition;
 
@@ -18,7 +18,7 @@ public class Flying : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(Vector3.up, itemRotationSpeed * Time.deltaTime, Space.World);
-        transform.position = basePosition + new Vector3(3.221f, 1.26f + 0.25f * Mathf.S(Time.time * itemBobSpeed), 0f);
+        transform.Rotate(Vector3.up, itemRotationSpeed/2 * Time.deltaTime, Space.World);
+        transform.position = basePosition + new Vector3(3.221f, 1.26f + 0.25f * Mathf.Cos(Time.time * itemBobSpeed), 0f);
     }
 }
