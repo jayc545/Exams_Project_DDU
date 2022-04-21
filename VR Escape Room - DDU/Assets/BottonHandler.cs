02a9aec93891e38cc912c39b39f4e4fd24edfea7
jayc545 <54627710+jayc545@users.noreply.github.com>
2022-04-21 23:39:07 +0200

    More

--- VR Escape Room - DDU/Assets/BottonHandler.cs
+++ VR Escape Room - DDU/Assets/BottonHandler.cs
@@ -2,11 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BottonHandler : MonoBehaviour
 {
+    public TMP_Text funnyText;
+
+    private void Start()
+    {
+        funnyText.enabled = false;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Restart()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+    public void QuitGame()
+    {
+        funnyText.enabled = true;
+       // Application.Quit();
     }
 }
